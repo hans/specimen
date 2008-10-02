fc6af4c7b84720d11cc2feab53a5f171c2295d48
Hans Engel <engel@engel.uk.to>
2008-10-01 17:30:26 -0700

    added base files for string

---
+++ php/string.php
@@ -0,0 +1,51 @@
+<?php
+
+// :specimen: string.basics //
+	// :basics: Set a variable to a string value //
+		
+	// end //
+	
+	// :basics: Escape a single quote //
+		
+	// end //
+	
+	// :basics: Multiline strings //
+		
+	// end //
+// end //
+
+// :specimen: string.substrings //
+	// :substrings: Retrieve parts of a string //
+		
+	// end //
+	
+	// :substrings: Modify parts of a string //
+		
+	// end //
+// end //
+
+// :specimen: string.defaults //
+	// :defaults: Set string defaults //
+		
+	// end //
+	
+	// :defaults: Set strings if not already set //
+		
+	// end //
+// end //
+
+// :specimen: string.general //
+	// :general: Reversing a string //
+		
+	// end //
+	
+	// :general: String case //
+		
+	// end //
+	
+	// :general: Trimming strings //
+		
+	// end //
+// end //
+
+?>
