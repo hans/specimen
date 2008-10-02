243f8eac7fc2e35507a6aa657e4f9c477694c1e3
Hans Engel <engel@engel.uk.to>
2008-10-01 17:40:36 -0700

    *.number.*; init

---
+++ c++/number.cpp
@@ -0,0 +1,17 @@
+// :specimen: string.general //
+	// :general: Round a floating-point number //
+		
+	// end //
+	
+	// :general: Increment a number //
+		
+	// end //
+	
+	// :general: Generate a random number //
+		
+	// end //
+	
+	// :general: Matrices //
+		
+	// end //
+// end //
