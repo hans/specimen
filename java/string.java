0e15a05c234ce88d8f3aab5e13ff935e9d84022e
Trevor Fountain <doches@gmail.com>
2008-10-02 12:09:47 +0100

    Added examples for everything except specimen.default, because I'm not sure that those examples really make sense for Java.

--- java/string.java
+++ java/string.java
@@ -1,24 +1,30 @@
 // :specimen: string.basics //
 	// :basics: Set a variable to a string value //
-		
+		String var = "A String!";
+		String var2 = new String("Another String!");
 	// end //
 	
 	// :basics: Escape a single quote //
-		
+		String var = "A \'quote\'";
 	// end //
 	
 	// :basics: Multiline strings //
-		
+		// Java doesn't allow multiline strings, but you can fake it using concat:
+		String multiline = "line 1 is a string "+
+		                   "that wraps to a new " +
+		                   "line a few times.";
 	// end //
 // end //
 
 // :specimen: string.substrings //
 	// :substrings: Retrieve parts of a string //
-		
+		String str = "xAAAxx";
+		String substr = str.substring(1,4); // Yields "AAA"
 	// end //
 	
 	// :substrings: Modify parts of a string //
-		
+	  // Strings are immutable in Java, so you can't modify a string in-place.
+
 	// end //
 // end //
 
@@ -34,14 +40,24 @@
 
 // :specimen: string.general //
 	// :general: Reversing a string //
-		
+    String ltor = "abcdef";
+    char c[] = ltor.toCharArray();
+    for(int i=0;i<c.length/2;i++) {
+      char t = c[i];
+      c[i] = c[c.length-i-1];
+      c[c.length-i-1] = t;
+    }
+    String rtol = new String(c);		
 	// end //
 	
 	// :general: String case //
-		
+		String mix = "aAbBcC";
+		String upper = mix.toUpperCase();
+		String lower = mix.toLowerCase();
 	// end //
 	
 	// :general: Trimming strings //
-		
+		String junk = " str \n";
+		String clean = junk.trim(); // Yields "str"
 	// end //
 // end //
