168f54ae9a05d3447a6fde0e5da13a75c5c8e3b1
Trevor Fountain <doches@gmail.com>
2008-10-02 14:09:49 +0100

    Examples for everything except string.defaults and modifying substrings -- all the documentation for PHP indicates that you shouldn't modify strings in place, if it's even possible.

--- php/string.php
+++ php/string.php
@@ -2,25 +2,33 @@
 
 // :specimen: string.basics //
 	// :basics: Set a variable to a string value //
-		
+		$str = "string";
+		$str = 'string';
 	// end //
 	
 	// :basics: Escape a single quote //
-		
+		$str = "\'quote\'";
 	// end //
 	
 	// :basics: Multiline strings //
-		
+		$str = <<<STR
+		A heredoc string, with 
+		the marker (STR in this case)
+		on a line by itself, with
+		no leading spaces.
+STR;
 	// end //
 // end //
 
 // :specimen: string.substrings //
 	// :substrings: Retrieve parts of a string //
-		
+		$str = "abcdefg";
+		$substr = substr($str,1,3); // substr(STRING, START, LENGTH);
+		                            // Yields "bcd"
 	// end //
 	
 	// :substrings: Modify parts of a string //
-		
+	
 	// end //
 // end //
 
@@ -36,15 +44,20 @@
 
 // :specimen: string.general //
 	// :general: Reversing a string //
-		
+		$str = "foo";
+		$rev = strrev($str); // Yields 'oof'
 	// end //
 	
 	// :general: String case //
-		
+		$foo = "aAbBcCdD";
+		$upper = strtoupper($foo);
+		$lower = strtolower($foo);
+		$caps = ucwords($foo); // or ucfirst($foo);
 	// end //
 	
 	// :general: Trimming strings //
-		
+		$foo = "  foo  \n";
+		$foo = trim($foo); // Yields "foo"
 	// end //
 // end //
 
