664df05b05283f91680660e437a29e14aab276a2
Hans Engel <engel@engel.uk.to>
2008-10-01 17:48:38 -0700

    *.date.*; init

---
+++ c#/date.cs
@@ -0,0 +1,23 @@
+// :specimen: date.general //
+	// :general: Retrieve the current date //
+		
+	// end //
+	
+	// :general: Sleeping //
+		
+	// end //
+// end //
+
+// :specimen: date.manipulation //
+	// :manipulation: Manipulating dates //
+		
+	// end //
+	
+	// :manipulation: Date conversion //
+		
+	// end //
+	
+	// :manipulation: Parse a date from a string //
+		
+	// end //
+// end //
---
+++ php/date.php
@@ -0,0 +1,27 @@
+<?php
+
+// :specimen: date.general //
+	// :general: Retrieve the current date //
+		
+	// end //
+	
+	// :general: Sleeping //
+		
+	// end //
+// end //
+
+// :specimen: date.manipulation //
+	// :manipulation: Manipulating dates //
+		
+	// end //
+	
+	// :manipulation: Date conversion //
+		
+	// end //
+	
+	// :manipulation: Parse a date from a string //
+		
+	// end //
+// end //
+
+?>
