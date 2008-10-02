50fe998de4b24fc2238f0b0f6460e26edd056971
Trevor Fountain <doches@gmail.com>
2008-10-02 14:05:05 +0100

    Basic examples for Fixnum

--- ruby/number.rb
+++ ruby/number.rb
@@ -1,17 +1,23 @@
 # :specimen: string.general #
 	# :general: Round a floating-point number #
-		
+		pi = 3.14
+		rough_pi = pi.round
 	# end #
 	
 	# :general: Increment a number #
-		
+		i = 1
+		i = i.next # => 2
+		i += 1 # => 3
 	# end #
 	
 	# :general: Generate a random number #
-		
+		real = rand # random real from [0,1)
+		integer = rand*10 # random int from [0,10)
 	# end #
 	
 	# :general: Matrices #
-		
+		matrix = [ [1, 0, 0],
+		           [0, 1, 0],
+		           [0, 0, 1] ] # => Identity matrix
 	# end #
 # end #
