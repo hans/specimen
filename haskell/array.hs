cc26444fe077c3602cfedf5dd5a2f8e8d1970a1c
Hans Engel <engel@engel.uk.to>
2008-10-01 17:57:40 -0700

    *.array.*; init

---
+++ haskell/array.hs
@@ -0,0 +1,27 @@
+-- :specimen: array.general --
+	-- :general: Creating arrays --
+		
+	-- end --
+	
+	-- :general: Looping through an array --
+		
+	-- end --
+-- end --
+
+-- :specimen: array.manipulation --
+	-- :general: Joining two arrays --
+		
+	-- end --
+	
+	-- :general: Reverse an array --
+		
+	-- end --
+	
+	-- :general: Sort an array --
+		
+	-- end --
+	
+	-- :general: Shuffle an array --
+		
+	-- end --
+-- end --
