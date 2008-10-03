61012a168dc64784fb8514e9ed587c1c11c22a98
Hans Engel <engel@engel.uk.to>
2008-10-02 19:49:52 -0700

    *.file.*; init

---
+++ c#/file.cs
@@ -0,0 +1,47 @@
+// :specimen: file.general //
+	// :general: Open a file //
+		
+	// end //
+	
+	// :general: File modes //
+		
+	// end //
+	
+	// :general: Expanding tildes in filenames //
+		
+	// end //
+// end //
+
+// :specimen: file.reading //
+	// :reading: Read a file line-by-line //
+		
+	// end //
+	
+	// :writing: Read an entire file into a variable //
+		
+	// end //
+// end //
+
+// :specimen: file.writing //
+	// :writing: Write to a file //
+		
+	// end //
+	
+	// :writing: Creating a lockfile //
+		
+	// end //
+	
+	// :writing: Flush output //
+		
+	// end //
+// end //
+
+// :specimen: file.temporary //
+	// :temporary: Create a temporary file //
+		
+	// end //
+	
+	// :temporary: Write to a temporary file //
+		
+	// end //
+// end //
