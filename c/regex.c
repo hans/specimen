fc635b587690b6adc24b56a14f93044601a843e1
Hans Engel <engel@engel.uk.to>
2008-10-02 19:38:34 -0700

    *.regex.*; init

---
+++ c/regex.c
@@ -0,0 +1,13 @@
+// :specimen: regex.general //
+	// :general: Basic matching with strings //
+		
+	// end //
+	
+	// :general: Regex replacements //
+		
+	// end //
+	
+	// :general: Checking for a valid email address //
+		
+	// end //
+// end //
---
+++ php/regex.php
@@ -0,0 +1,17 @@
+<?php
+
+// :specimen: regex.general //
+	// :general: Basic matching with strings //
+		
+	// end //
+	
+	// :general: Regex replacements //
+		
+	// end //
+	
+	// :general: Checking for a valid email address //
+		
+	// end //
+// end //
+
+?>
