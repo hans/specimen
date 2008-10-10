af08bf33bac6c534fc2cc75256f61f4dd00fe61d
Trevor Fountain <doches@gmail.com>
2008-10-10 15:40:48 +0100

    Everything except matrices.

--- c++/number.cpp
+++ c++/number.cpp
@@ -1,14 +1,21 @@
 // :specimen: string.general //
 	// :general: Round a floating-point number //
-		
+	  #include <math.h>
+		float n = 0.4f;
+		n = round(n);
 	// end //
 	
 	// :general: Increment a number //
-		
+		int m = 0;
+		m++;
 	// end //
 	
 	// :general: Generate a random number //
+		#include <time.h>
+    #include <stdlib.h>
 		
+		srandom(time(NULL));
+		int o = random() % 10; // => integer in the interval [0,10]
 	// end //
 	
 	// :general: Matrices //
